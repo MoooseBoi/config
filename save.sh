9f57b5cc15e8082ccb6a4c06243037a75c405717
Moose <mooooseboi@gmail.com>
2024-09-27 00:08:07 +0300

    Backup on 2024-09-27_00-08-02

--- save.sh
+++ save.sh
@@ -16,9 +16,14 @@ done
 
 git diff
 
+read -p "do you want to save changes? [Y/N]: " answer
+if [ ! $answer = "Y" ]; then
+  exit 0
+fi
+
 git add .
 git commit -m "Backup on $DATE"
-# git push origin main
+git push origin main
 
 echo "Backup complete and changes pushed!"
 
